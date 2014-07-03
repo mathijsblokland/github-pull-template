e46a8ae59e255e92d347da36fd4dae8776d21f8a
Mathijs Blokland <mathijs.blokland@eduhub.nl>
2014-07-03 15:02:25 +0200

    Initial commit

---
+++ src/github-pr-template.js
@@ -0,0 +1,42 @@
+(function($) {
+  setPrBody = function(){
+    if ($('#commits_bucket').length > 0){
+      // Stop interval if we detect the #commits_bucket
+      clearInterval(handle);
+      handle = 0;
+
+      // Get context from the URL
+      var baseUrl = window.location.href.split( '/' ),
+          mergePath = baseUrl[6].split( '...' ),
+          branchBase = mergePath[0],
+          branchHead = mergePath[1],
+          prBody = $('textarea[name="pull_request[body]"]'),
+          prBodyValue = prBody.val();
+
+      // Set default PR template
+      var data = "#### What's this PR do?\n\n#### Where should the reviewer start?\n\n#### How should this be manually tested?\n\n#### Any background context you want to provide?\n\n#### Screenshots (if appropriate)\n\n#### Important links:\n* _Asana issue(s):_ [`#`]()\n* _CodeClimate's comparison:_ [`" + branchBase + "` ... `" + branchHead + "`](https://codeclimate.com/repos/504f5356f3ea005e1c004209/compare/" + branchHead + ")\n\n#### Questions:\n  - Do we need to translate new strings?\n  - Does anyone need to be notified when this has been deployed?\n  - Does this include migrations that might take a while?";
+
+      // If there is a default template remove it (can happen due local storage)
+      if(prBodyValue.indexOf("#### What's this PR do?\n\n#### Where should") >= 0){
+        prBodyValue = '';
+      }
+
+      // Add extra whitespace before default PR template if there is content
+      if(prBodyValue != ''){
+        data = '\n\n' + data;
+      }
+
+      // Set new prBody
+      prBody.val(prBodyValue + data);
+      prBody.css('height', '475px');
+    }
+  }
+
+  // Set interval to detect if we are creating a PR
+  var handle = setInterval(function() { setPrBody() }, 1000);
+
+  // Bind an onclick for all pjax calls
+  $('body').on('click', 'a[data-pjax]', function(){
+    setTimeout(function(){ setPrBody() }, 1000)
+  })
+})(jQuery)
